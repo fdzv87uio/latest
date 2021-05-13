fecc488f0ea86d29c0e25fbf8b46e2c5806a1c6a
@fausto1 <fausto@parts-badger.com>
2021-05-13 13:56:39 -0500

    camera layout

--- pages/pose-estimation.tsx
+++ pages/pose-estimation.tsx
@@ -95,8 +95,8 @@ const PoseEstimation = observer(() => {
     ) {
       // Get Video Properties
       const video = camRef.current.video
-      const videoWidth = 400
-      const videoHeight = 400
+      const videoWidth = 500
+      const videoHeight = 500
       console.log(videoWidth)
       // Make detections
       const pose = await net.estimateSinglePose(video)
@@ -125,8 +125,8 @@ const PoseEstimation = observer(() => {
       <S.PageWrapper>
         <Webcam
           audio={false}
-          height={400}
-          width={400}
+          height={500}
+          width={500}
           ref={camRef}
           screenshotFormat="image/jpeg"
         />
@@ -142,13 +142,13 @@ const PoseEstimation = observer(() => {
               right: 0,
               textAlign: 'center',
               zIndex: 9,
-              width: 400,
-              height: 400,
+              width: 500,
+              height: 500,
             }}
           />
         ) : null}
         {permissionGranted === true ? (
-          <Canvas width={400} height={400} dpr={1} isAnimating={true}>
+          <Canvas width={500} height={500} dpr={1} isAnimating={true}>
             <OrientationAxis
               beta={deviceOrientation?.beta}
               gamma={deviceOrientation?.gamma}
