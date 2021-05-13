5ef75bd06285cd8b10281b9c89220b43c7749d5c
@fausto1 <fausto@parts-badger.com>
2021-05-13 14:53:23 -0500

    camera layout

--- styles/pose_estimation.styles.tsx
+++ styles/pose_estimation.styles.tsx
@@ -3,10 +3,10 @@ import Styled from 'styled-components'
 export const PageWrapper = Styled.div`
 
     display: grid;
-    grid-template-columns: 100%;
+    grid-template-columns: 120%;
     background-color: #ffffff;
     width:100%;
-    height:100%;
+    height:auto;
 
     .row .column:first-child {
   padding-left: 0;
