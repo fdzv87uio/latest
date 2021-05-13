cb289dc3c8220f8f2007f474a5a492b04f466420
@fausto1 <fausto@parts-badger.com>
2021-05-13 17:43:19 -0500

    camera layout

--- styles/pose_estimation.styles.tsx
+++ styles/pose_estimation.styles.tsx
@@ -2,16 +2,27 @@ import Styled from 'styled-components'
 
 export const PageWrapper = Styled.div`
 
-    display: block;
+    display: grid;
+    grid-template-columns: 100%;
+    justify-items: center;
     background-color: #ffffff;
-    width:100vh;
+    width:100%;
     height:100%;
 
-
-
-video {
-    width:100vh;
-    height:100%
+    .row .column:first-child {
+  padding-left: 0;
 }
+
+.row .column:last-child {
+  padding-right: 0;
+}
+
+    video{
+        width:100vh;
+        height:100%;
+    }
+
+
+
     
 `
