1f026e3b276dd43d2344e080184c1428f5073ed6
@fausto1 <fausto@parts-badger.com>
2021-05-13 17:59:13 -0500

    camera layout

--- styles/pose_estimation.styles.tsx
+++ styles/pose_estimation.styles.tsx
@@ -7,17 +7,9 @@ export const PageWrapper = Styled.div`
     width:100vh;
     height:100%;
 
-    .row .column:first-child {
-  padding-left: 0;
-}
-
-.row .column:last-child {
-  padding-right: 0;
-}
 
     video{
         width:100vh;
-        height:100%;
     }
 
 
